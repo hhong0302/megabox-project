7a6d641d8aa5f96f19b5d96dfc5ad25893260d9b
hhong0302 <wonhong0302@naver.com>
2023-04-13 23:59:34 +0900

    workindesktop

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ function hong_delete()
 {
     document.getElementsByClassName("hong-advertisement")[0].classList.add('hong-ad-none');
 }
+
+//hamburger 버튼 클릭 시 사이트맵 나왔다 들어갔다
+
+function hong_sitemap_onout()
+{
+    var st = document.getElementsByClassName("hong-sitemap")[0];
+    var xbtn = document.getElementsByClassName("hong-btn-close")[0];
+    if(st.style.display=="block")
+    {
+        st.style.display="none";
+        xbtn.style.display="none";
+    }
+    else
+    {
+        st.style.display = "block";
+        xbtn.style.display="block";
+    }
+}

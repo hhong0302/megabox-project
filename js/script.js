137774887ff9e5badf97820b098e1ff55210de00
hhong0302 <wonhong0302@naver.com>
2023-04-17 23:53:09 +0900

    0417desktop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 //광고 x 버튼 누르면 광고 닫히기
-
 function hong_delete()
 {
     document.getElementsByClassName("hong-advertisement")[0].classList.add('hong-ad-none');
 }
 
+//버튼들
 const hong_st = document.getElementsByClassName("hong-sitemap")[0];
 const hong_xbtn1 = document.getElementsByClassName("hong-btn-close")[0];
 const hong_search = document.getElementsByClassName('hong-search-map')[0];
 const hong_xbtn2 = document.getElementsByClassName("hong-btn-close")[1];
+const hong_member = document.getElementsByClassName('hong-member-map')[0];
+const hong_xbtn3 = document.getElementsByClassName("hong-btn-close")[2];
 
 //hamburger 버튼 클릭 시 사이트맵 나왔다 들어갔다
 
@@ -27,6 +29,11 @@ function hong_sitemap_onout()
         {
             hong_search.style.display="none";
             hong_xbtn2.style.display="none";
+        }
+        if(hong_member.style.display=="block")
+        {
+            hong_member.style.display="none";
+            hong_xbtn3.style.display="none";
         }
     }
 }
@@ -50,10 +57,41 @@ function hong_search_onout()
             hong_st.style.display="none";
             hong_xbtn1.style.display="none";
         }
+        if(hong_member.style.display=="block")
+        {
+            hong_member.style.display="none";
+            hong_xbtn3.style.display="none";
+        }
+    }
+}
+
+//member 버튼 클릭 시 member맵 나왔다 들어갔다
+function hong_member_onout()
+{
+    if(hong_member.style.display=="block")
+    {
+        hong_member.style.display="none";
+        hong_xbtn3.style.display="none";
+    }
+    else
+    {
+        hong_member.style.display = "block";
+        hong_xbtn3.style.display="block";
+        if(hong_st.style.display=="block")
+        {
+            hong_st.style.display="none";
+            hong_xbtn1.style.display="none";
+        }
+        if(hong_search.style.display=="block")
+        {
+            hong_search.style.display="none";
+            hong_xbtn2.style.display="none";
+        }
     }
 }
 
 //네비게이션 메뉴에 갖다대면 바뀌는 그거
+
 const hong_list = document.getElementsByClassName('hong-navbar');
 const hong_navbar_list = document.getElementsByClassName('hong-navbar-list');
 const hong_nav_list = document.getElementsByClassName('hong-nav-list');
@@ -92,18 +130,12 @@ hong_nav_store.onmouseover =()=>
 //이미지에 마우스 갖다대면 바뀌는 그거
 
 const hong_map_link = document.getElementsByClassName('hong_map_link');
-/* 
-for(let i = 0 ; i<hong_map_link.length ; i++)
-{
-    hong_map_link[i].addEventListener('mouseenter',function()
-    { */
+
 function hong_map_mouseenter(hong_attr)
 {
     document.getElementsByClassName('hong_map_image')[0].setAttribute("src" , "images/hong-map-"+hong_attr+".jpg");
     document.getElementsByClassName('hong_map_image')[1].setAttribute("src" , "images/hong-map-"+hong_attr+".jpg");
 }
-/*     });
-} */
 
 //search-map ticket과 client 누르면 바뀌기
 

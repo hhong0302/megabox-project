8dfb8759f3b967680556c81c6e61f84672acdd2a
hhong0302 <wonhong0302@naver.com>
2023-04-17 23:55:22 +0900

    just commit

--- js/script.js
+++ js/script.js
@@ -205,3 +205,14 @@ hong_body.addEventListener('click',(event)=>
         hong_xbtn2.style.display="none";
     }
 }); */
+
+//login form by jy
+$(function(){
+    $('.hong-login-link').click(function(e){
+        e.preventDefault();
+        $('.cinemalogin-fade').show();
+    });
+    $('.cinemalogin-close').click(function(){
+        $('.cinemalogin-fade').hide();
+    });
+});

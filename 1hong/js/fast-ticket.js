7349f83859fd78d78af2c0a0d14697b3ca34cf89
hhong0302 <wonhong0302@naver.com>
2023-04-24 00:05:40 +0900

    0423 home

--- 1hong/js/fast-ticket.js
+++ 1hong/js/fast-ticket.js
@@ -1,9 +1,11 @@
+
+//오늘날짜 부터 나열해서 예매박스 상단 날짜부분에 넣기
 let dt = new Date();
 let year = dt.getFullYear();
 let month = dt.getMonth();  //3
 let date = dt.getDate();  //23
-/* let day = dt.getDay();  //0
-console.log(year+"'"+month+"'"+date+"'"+day); */
+//let day = dt.getDay();  //0
+//console.log(year+"'"+month+"'"+date+"'"+day);
 
 let li = "";
 let dayarr = ["일","월","화","수","목","금","토"];
@@ -14,8 +16,11 @@ for(let i=0;i<12;i++)
     let d_day = d.getDay();
     li+=`<button class="hong-ticket-box-list-button hong-button">
             <span>${d.getDate()}</span>
-            <span>${dayarr[d_day]}</span>
+            <span class="hong-day">${dayarr[d_day]}</span>
         </button>`;
 }
-
 document.getElementsByClassName('hong-ticket-box-date-list')[0].innerHTML = li;
+
+const hd = document.getElementsByClassName("hong-day");
+hd[0].innerHTML = '오늘';
+hd[1].innerHTML = '내일';
